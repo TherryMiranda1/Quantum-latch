df7aba551b33ccdf26e0f1fe1e598c1720122a08
laptop haru <therrymiranda1@gmail.com>
2024-11-28 02:51:52 +0000

    add images

--- web/src/components/widgets/Timeline/Timeline.tsx
+++ web/src/components/widgets/Timeline/Timeline.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@/components/ui";
+import { DEVICE_BREAKPOINTS } from "@/constants/devices";
 import styled from "styled-components";
 
 export interface Timeline {
   id: string;
   title: string;
   description?: string;
+  image?: string;
+  image2?: string;
   time?: string;
   icon?: JSX.Element;
 }
@@ -27,6 +30,10 @@ export const Timeline = ({ data }: Props) => {
             {item.time && <span>{item.time}</span>}
             <Header componentType="h3" text={item.title} />
             {item.description && <p>{item.description}</p>}
+            <div>
+              {item.image && <TimelineImage src={item.image} />}
+              {item.image2 && <TimelineImage src={item.image2} />}
+            </div>
           </ContentStyled>
         </TimeItemStyled>
       ))}
@@ -96,4 +103,29 @@ const ContentStyled = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4px;
+
+  div {
+    margin: 16px 0;
+    box-sizing: border-box;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+  }
+`;
+
+const TimelineImage = styled.img`
+  width: 500px;
+  max-width: 70%;
+  max-height: 700px;
+  object-fit: cover;
+  object-position: top;
+  border-radius: var(--small-radius);
+  border: 1px solid var(--bg-200);
+
+  @media screen and (max-width: ${DEVICE_BREAKPOINTS.md}) {
+    max-width: 100%;
+  }
 `;
--- web/src/components/widgets/Timeline/demo.ts
+++ web/src/components/widgets/Timeline/demo.ts
@@ -3,20 +3,18 @@ export const DEMO_DATA = [
     id: "1",
     time: "Paso 1",
     title: "Conecta tu cuenta de GitHub",
+    image:
+      "https://res.cloudinary.com/dzkcloud/image/upload/v1732758766/quantum/github_0_qtwudc.png",
     description:
       "Inicia sesión en la plataforma Quantum y conecta tu cuenta de GitHub utilizando el flujo de autenticación OAuth. Esto permite a Quantum acceder a los repositorios que deseas gestionar.",
   },
+
   {
     id: "2",
     time: "Paso 2",
-    title: "Seleccióna el repositorio",
-    description:
-      "Selecciona el repositorio que deseas proteger. Quantum solicitará los permisos necesarios para gestionar las integraciones y aplicar los pestillos de seguridad en ese repositorio.",
-  },
-  {
-    id: "3",
-    time: "Paso 3",
     title: "Instala la aplicación Quantum",
+    image:
+      "https://res.cloudinary.com/dzkcloud/image/upload/v1732758766/quantum/1_yri4gj.png",
     description:
       "Instala la aplicación Quantum en el repositorio seleccionado. Este paso asegura que las operaciones críticas del repositorio estarán vinculadas a los pestillos de seguridad gestionados por Quantum.",
   },
@@ -24,13 +22,27 @@ export const DEMO_DATA = [
     id: "4",
     time: "Paso 4",
     title: "Vincula los pestillos",
+    image:
+      "https://res.cloudinary.com/dzkcloud/image/upload/v1732758767/quantum/photo_2024-11-28_01-43-48_ulunwp.jpg",
     description:
       "Vincula un pestillo de Latch al repositorio seleccionado. Cada pestillo controla un aspecto crítico como la fusión de Pull Requests, los despliegues o cambios en la configuración.",
+  },
+  {
+    id: "5",
+    time: "Paso 5",
+    title: "Seleccióna el repositorio",
+    image:
+      "https://res.cloudinary.com/dzkcloud/image/upload/v1732758767/quantum/github_2_v7yezm.png",
+    image2:
+      "https://res.cloudinary.com/dzkcloud/image/upload/v1732758767/quantum/github_4_kxvzfr.png",
+    description:
+      "Selecciona el repositorio que deseas proteger. Quantum solicitará los permisos necesarios para gestionar las integraciones y aplicar los pestillos de seguridad en ese repositorio.",
   },
   {
     id: "6",
     time: "Paso 6",
     title: "Ya lo tienes ",
+    image: "https://res.cloudinary.com/dzkcloud/image/upload/v1732758767/quantum/photo_2024-11-28_01-43-48_2_wm0d9f.jpg",
     description:
       "Desde tu app de latch, revisa el estado de los pestillos, visualiza intentos de acceso o cambios no autorizados, y ajusta las configuraciones según sea necesario.",
   },
